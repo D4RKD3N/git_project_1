9d368f80dfb64fe9caebf716aa8cb0fbdbf757a6
D4RKD3N <57715139+D4RKD3N@users.noreply.github.com>
2019-11-13 16:37:17 +0300

    Update program.py
    NEW COMMIT

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every year')
     print('New commit from Pycharm')
+    print('New commit from GitHub')
 
 if __name__ == '__main__':
     main()
